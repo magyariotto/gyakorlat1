eb4c86aab83e7887631f176ccf8605926f7e5434
magyariotto <magyariotto96@gmail.com>
2020-11-18 19:48:10 +0200

    For Loop 18.11.2020 19.48

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,91 +1,58 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Application {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] intArray = new int[]{1, 2, 3, 4};
 
-        System.out.println("Elso szam");
-        Integer szam1 = scanner.nextInt();
+        System.out.println("Array by index");
+        System.out.println(intArray[0]);
+        System.out.println(intArray[1]);
+        System.out.println(intArray[2]);
+        System.out.println(intArray[3]);
 
-        System.out.println("Operandus");
-        String operandus = new Scanner(System.in).nextLine();
-
-        System.out.println("Masodik szam");
-        Integer szam2 = scanner.nextInt();
-
-        System.out.println("Ezt a muveletet fogjuk elvegezni:" + szam1 + " "+ operandus + " " + szam2);
-
-        Double eredmeny = null;
-        if("+".equals(operandus)){
-            eredmeny = (double) szam1 + szam2;
-        }else if("-".equals(operandus)){
-            eredmeny = (double) szam1 - szam2;
-        }else if("*".equals(operandus)){
-            eredmeny = (double) szam1 * szam2;
-        }else{
-            System.out.println("Ismeretlen operandus:" + operandus);
-        }
-
-        System.out.println("Eredmeny: " + eredmeny);
-        if(szam1 < 10){
-            System.out.println("Az elso szam tiznel kisebb");
-        }
-
-        if(szam1 < 10 || szam2 < 10){
-            System.out.println("Valamelyik szam kisebb mint 10");
-        }
-
-        System.out.println("Teszt &&:");
-        if(hamis() && igaz()){
-            System.out.println("Ennek nem szaba kiirodnia");
-        }
-
-        System.out.println("Teszt &:");
-        if(igaz() & hamis()){
-            System.out.println("Ennek nem szaba kiirodnia");
-        }
-
-        System.out.println("Teszt ||:");
-        if(igaz() || hamis()){
-            System.out.println("Az egyik feltetel igaz volt");
-        }
-
-        System.out.println("Teszt |:");
-        if(igaz() | hamis()){
-            System.out.println("Az egyik feltetel igaz volt");
+        System.out.println();
+        System.out.println("Array with for");
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(i + "-edik elem a tombben: " +intArray[i]);
         }
 
         System.out.println();
-        System.out.println();
-        System.out.println();
-
-        Double eredmeny2 = null;
-        switch (operandus){
-            case "+":
-                eredmeny2 = (double) szam1 + szam2;
-                break;
-            case "-":
-                eredmeny2 = (double) szam1 - szam2;
-                break;
-            default:
-                System.out.println("Ismeretlen operandus: " + operandus);
+        System.out.println("Array enhanced with for");
+        for(int elem : intArray){
+            System.out.println("Elem: " + elem);
         }
-        System.out.println("A switch eredmeny: " + eredmeny2);
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Egy");
+        stringList.add("Ketto");
+        stringList.add("Harom");
+        stringList.add("Negy");
+
+        for(int i = 0; i<stringList.size(); i+=2){
+            System.out.println(i + "-edik elem a listaban: " + stringList.get(i));
+        }
 
         System.out.println();
-        System.out.println("Ird be a vizsgalando szamot");
-        String bemenet = new Scanner(System.in).nextLine();
-        boolean hoszabbMintHarom = bemenet.length() < 3;
-        String eredmeny3 =  hoszabbMintHarom ? "Hoszabb mint 3" : "Rovidebb mint 3";
-        System.out.println(eredmeny3);
+        System.out.println("List with enhanced for");
+        for(String elem : stringList){
+            System.out.println("Elem: " + elem);
+        }
+        //**************************************************
 
-    }
-    private static boolean igaz(){
-        System.out.println("Az igaz meg lett hivva");
-        return true;
-    }
-    private  static  boolean hamis(){
-        System.out.println("A hamis meglett hivva");
-        return  false;
+        Map<Integer, String> integerStringMap = new HashMap<>();
+        integerStringMap.put(1, "Egy");
+        integerStringMap.put(2, "Ketto");
+        integerStringMap.put(3, "Harom");
+        integerStringMap.put(4, "Negy");
+
+        System.out.println();
+        System.out.println("Map with enhanced for");
+        for(Map.Entry<Integer, String> entry : integerStringMap.entrySet()){
+
+            System.out.println("Kulcs: " + entry.getKey() + ", value: " +entry.getValue());
+        }
     }
 }
