d9a0273a0b95cc737bbcc80c244135897024bd68
magyariotto <magyariotto96@gmail.com>
2020-11-21 15:16:19 +0200

    Homework 1 21.11.2020 15.16

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1.Feladat:");
+        System.out.println("Irj be egy mondatot,es megmondom a leghoszabb szot belole.");
+        String szoveg = scanner.nextLine();
+        String[] darabok = szoveg.split(" ");
+        Integer szavakSzama = darabok.length;
+        Integer valtozo = 0;
+        Integer leghosszabbSzo = 0;
 
+        for(int i = 0; i< szavakSzama; i++){
+            Integer szo = darabok[i].length();
+            if( szo > leghosszabbSzo){
+                leghosszabbSzo = szo;
+                valtozo = i;
+            }
+        }
+        System.out.println(darabok[valtozo]);
     }
 }
