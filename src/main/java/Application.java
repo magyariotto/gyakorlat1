f3e33b3b3cdcafcf49bf7176e779ceb049d601ec
magyariotto <magyariotto96@gmail.com>
2020-11-21 15:37:38 +0200

    Homework 1,2 21.11.2020 15.37

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -19,5 +19,17 @@ public class Application {
             }
         }
         System.out.println(darabok[valtozo]);
+
+        System.out.println();
+        System.out.println("2. Feladat:");
+        System.out.println("Kerlek adj meg egy szoveget,es megforditom;");
+
+        String szoveg2 = scanner.nextLine();
+        String[] tomb = szoveg2.split("");
+        Integer tombHossza = tomb.length;
+
+        for(int i=0; i<tombHossza; i++){
+            System.out.print(tomb[tombHossza-i-1]);
+        }
     }
 }
