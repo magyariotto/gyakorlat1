a95dde850dfc3dc7f6c4dcbe02d48945060f0085
magyariotto <magyariotto96@gmail.com>
2020-12-01 17:59:00 +0200

    Maven 29.11.2020

--- src/main/java/com/github/magyariotto/Application.java
+++ src/main/java/com/github/magyariotto/Application.java
@@ -1,10 +1,7 @@
 package com.github.magyariotto;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Application {
     public static void main(String[] args) {
-        boolean result = StringUtils.isNoneBlank("afe");
-        System.out.println("Eredmeny: " + result);
+
     }
 }
