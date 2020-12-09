307e782a7cd3f4aabd032996f472c28b2cff72a9
magyariotto <magyariotto96@gmail.com>
2020-12-09 20:38:29 +0200

    Hello world!+Input request Spring Controller 09.12.2020 20.04

--- src/main/java/com/github/magyariotto/controller/PageController.java
+++ src/main/java/com/github/magyariotto/controller/PageController.java
@@ -1,8 +1,11 @@
 package com.github.magyariotto.controller;
 
+import com.github.magyariotto.request.UsernameRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -10,5 +13,13 @@ public class PageController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/input")
+    public ModelAndView input(UsernameRequest request) {
+        log.info("/input - request: {}", request);
+        ModelAndView mav = new ModelAndView("input");
+        mav.addObject("username", request.getUsername());
+        return mav;
     }
 }
---
+++ src/main/java/com/github/magyariotto/request/UsernameRequest.java
@@ -0,0 +1,10 @@
+package com.github.magyariotto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UsernameRequest {
+    public String username;
+}
